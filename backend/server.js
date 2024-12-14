357ad08cdd6aaec7335a66b5d92e0f6f9e5b99ee
Aashish pandey <pandey786aashish@gmail.com>
2024-12-14 07:43:04 +0530

    we successfully completed Webhooks  integraction

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './config/mongodb.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 const PORT=process.env.PORT || 4000
@@ -13,6 +14,7 @@ app.use(cors());
 
 // Routes
 app.get('/', (req, res) => res.send("API Working"));
+app.use('/api/user',userRouter)
 
 
 app.listen(PORT,()=>{
