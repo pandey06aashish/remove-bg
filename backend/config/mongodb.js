3b670699682076124fb44b0de99c14687c84ae12
Aashish pandey <pandey786aashish@gmail.com>
2024-12-14 06:32:53 +0530

    correct syntax error in mongoose folder

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,25 +1,11 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  try {
-    // Listen for successful connection events
-    mongoose.connection.on("connected", () => {
-      console.log(`Database Connected: ${mongoose.connection.host}`);
-    });
-
-    // Listen for disconnection events
-    mongoose.connection.on("disconnected", () => {
-      console.log("Database Disconnected");
-    });
-
-    // Attempt connection
-    await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "remove-bg", // Specify the database name
-    });
-  } catch (error) {
-    console.error(`Error connecting to the database: ${error.message}`);
-    process.exit(1); // Exit the process with a failure code
-  }
-};
+const connectDB=async ()=>{
+    mongoose.connection.on('connected',()=>{
+      console.log("Database Connected");
+      
+    })
+    await mongoose.connect(`${process.env.MONGODB_URI}/remove-bg`)
+}
 
 export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import mongoose from 'mongoose';
 import connectDB from './config/mongodb.js';
 
 const app = express();
+const PORT=process.env.PORT || 4000
 await connectDB();
 
 // Middlewares
@@ -14,5 +14,7 @@ app.use(cors());
 // Routes
 app.get('/', (req, res) => res.send("API Working"));
 
-// Export the app for Vercel
-export default app;
+
+app.listen(PORT,()=>{
+    console.log("Server Start on Port:"+PORT);
+})
